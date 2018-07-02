5167e8ca4fc4cb55cc060dd5d8f1c81859491af8
leoytliu <leoytliu@tencent.com>
2018-07-02 19:59:14 +0800

    Prettier

--- src/extension.ts
+++ src/extension.ts
@@ -1,22 +1,21 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the necessary extensibility types to use in your code below
-import {ExtensionContext} from 'vscode';
-import {
-    ProgressWatcher
-} from './progress'
+import { ExtensionContext } from "vscode";
+import { ProgressWatcher } from "./progress";
 
 // This method is called when your extension is activated. Activation is
 // controlled by the activation events defined in package.json.
 export function activate(context: ExtensionContext) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error).
+  // This line of code will only be executed once when your extension is activated.
+  console.log(
+    'Congratulations, your extension "webpack-progress-log" is now active!'
+  );
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error).
-    // This line of code will only be executed once when your extension is activated.
-    console.log('Congratulations, your extension "webpack-progress-log" is now active!');
+  // create a new word counter
+  let watcher = new ProgressWatcher();
+  watcher.initializeWatcher();
 
-    // create a new word counter
-    let watcher = new ProgressWatcher();
-    watcher.initializeWatcher();
-
-    // Add to a list of disposables which are disposed when this extension is deactivated.
-    context.subscriptions.push(watcher);
+  // Add to a list of disposables which are disposed when this extension is deactivated.
+  context.subscriptions.push(watcher);
 }
--- src/progress.ts
+++ src/progress.ts
@@ -1,64 +1,71 @@
-import * as vscode from 'vscode'
-import * as fs from 'fs'
-import * as os from 'os'
-import { EventEmitter } from 'events'
+import * as vscode from "vscode";
+import * as fs from "fs";
+import * as os from "os";
+import { EventEmitter } from "events";
 
 export class ProgressWatcher extends EventEmitter {
-    private timer: NodeJS.Timer
-    private statusBarItem: vscode.StatusBarItem
-    private progress: vscode.Progress<number>
-    private watcher: fs.FSWatcher
-    private initialLabel: string = 'webpack not running'
-    initializeWatcher() {
-        let tempFile = `${os.tmpdir()}/webpack-progress-log`
-        let lastModify = new Date(200, 10, 10) // initial date
-        vscode.window.withProgress({ location: vscode.ProgressLocation.Window, title: 'webpack progress'}, (p)=>{
-            return new Promise((resolve, reject) => {
-                p.report({message: 'Start working...' })
-                this.on('progressEnd', ()=>{
-                    resolve()
-                })
-                this.on('progressError', ()=>{
-                    reject()
-                })
-            })
-        })
-        this.watcher = fs.watch(tempFile, (eventType) => {
-            console.log(`event type is: ${eventType}`)
-            if (eventType === 'error') {
-                this.watcher.close()
-                this.emit('progressError')
-                console.error('Watch temp file error')
-            } else if (eventType === 'change') {
-                let content = fs.readFileSync(tempFile).toString().split(',')
-                let percentage = parseFloat(content[0]) || 0
-                let msg = content[1] || ''
-                
-            }
-        })
-    }
+  private timer: NodeJS.Timer;
+  private statusBarItem: vscode.StatusBarItem;
+  private progress: vscode.Progress<number>;
+  private watcher: fs.FSWatcher;
+  private initialLabel: string = "webpack not running";
+  initializeWatcher() {
+    let tempFile = `${os.tmpdir()}/webpack-progress-log`;
+    let lastModify = new Date(200, 10, 10); // initial date
+    vscode.window.withProgress(
+      { location: vscode.ProgressLocation.Window, title: "webpack progress" },
+      p => {
+        return new Promise((resolve, reject) => {
+          p.report({ message: "Start working..." });
+          this.on("progressEnd", () => {
+            resolve();
+          });
+          this.on("progressError", () => {
+            reject();
+          });
+        });
+      }
+    );
+    this.watcher = fs.watch(tempFile, eventType => {
+      console.log(`event type is: ${eventType}`);
+      if (eventType === "error") {
+        this.watcher.close();
+        this.emit("progressError");
+        console.error("Watch temp file error");
+      } else if (eventType === "change") {
+        let content = fs
+          .readFileSync(tempFile)
+          .toString()
+          .split(",");
+        let percentage = parseFloat(content[0]) || 0;
+        let msg = content[1] || "";
+      }
+    });
+  }
 
-    updateProgress(percentage:number, msg:string) {
-        if (!this.statusBarItem) {
-            this.statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left)
-            this.statusBarItem.text = this.initialLabel
-            this.statusBarItem.show()
-        }
-        let formatPercentage = (percentage*100).toFixed(2) 
-        this.statusBarItem.text = `${formatPercentage}% ${msg}`
-        if (percentage == 100 ) {
-            this.resetProgress()
-        }
+  updateProgress(percentage: number, msg: string) {
+    if (!this.statusBarItem) {
+      this.statusBarItem = vscode.window.createStatusBarItem(
+        vscode.StatusBarAlignment.Left
+      );
+      this.statusBarItem.text = this.initialLabel;
+      this.statusBarItem.show();
     }
+    let formatPercentage = (percentage * 100).toFixed(2);
+    this.statusBarItem.text = `${formatPercentage}% ${msg}`;
+    if (percentage == 100) {
+      this.resetProgress();
+    }
+  }
 
-    resetProgress() {
-        this.watcher.close()
-        this.statusBarItem.text = this.initialLabel
-        this.emit('progressEnd')
-    }
+  resetProgress() {
+    this.watcher.close();
+    this.statusBarItem.text = this.initialLabel;
+    this.emit("progressEnd");
+  }
 
-    dispose() {
-        this.statusBarItem.dispose()
-        this.watcher.close()
-    }
+  dispose() {
+    this.statusBarItem.dispose();
+    this.watcher.close();
+  }
 }
